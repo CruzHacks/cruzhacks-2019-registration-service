bd85a35af61cb1ecb357de6b10826b40a4658408
Kyle1668 <kyledevinobrien1@gmail.com>
2018-07-29 12:06:40 -0700

    Added home endpoint unit tests

--- src/tests/api/test_app.py
+++ src/tests/api/test_app.py
@@ -1,1 +1,13 @@
-# Todo
+
+"""[summary]
+"""
+
+from src.app import home
+
+
+def test_home_enpoint():
+    """[summary]`
+    """
+    res = home()
+    assert res.message == "Hello, World."
+    assert res.code == "200"

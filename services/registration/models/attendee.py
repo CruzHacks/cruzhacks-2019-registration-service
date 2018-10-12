f6bffea14f5eb7c304aac5f1dc2b5da0deaefd80
Alex Lue <alue@ucsc.edu>
2018-10-12 12:51:07 -0700

    updated db orm with more fields, req and opt

--- services/registration/models/attendee.py
+++ services/registration/models/attendee.py
@@ -20,17 +20,32 @@ class Attendee(DB.Model):
     """Table of a people's submitted registration info."""
     __tablename__ = 'registrations'
 
-    private_id = DB.Column('private_id', DB.String(36), primary_key=True)
+    # required fields
+    private_id = DB.Column('private_id', DB.String(36), primary_key=True, nullable = False)
     public_id = DB.Column('public_id', DB.Integer, unique=True, nullable=False)
-    email = DB.Column('email', DB.String(50), unique = True)
-    first_name = DB.Column('first_name', DB.String(50))
-    last_name = DB.Column('last_name', DB.String(50))
-    school = DB.Column('school', DB.String(50))
-    url = DB.Column('url', DB.String(100), nullable=True)
+    email = DB.Column('email', DB.String(50), unique = True, nullable=False)
+    first_name = DB.Column('first_name', DB.String(20), nullable=False)
+    last_name = DB.Column('last_name', DB.String(20), nullable=False)
+    university = DB.Column('school', DB.String(50), nullable=False)
+    birthday = DB.Column('birthday', DB.String(10), nullable=False) # MM-DD-YYYY
+    size = DB.Column('t-shirt_size', DB.String(5), nullable=False)
+    short_answer = DB.Column("short_answer", DB.String(250), nullable=False)
+
+    # optional fields
+    optional_info = {}
+    optional_info["gender"] = DB.Column('gender', DB.String(1)) #M or F or O (other or prefer not to say?)
+    optional_info["ethnicity"] = DB.Column('ethnicity',DB.String(20))
+    optional_info["major"] = DB.Column('major', DB.String(20))
+    optional_info["dietary_rest"] = DB.Column("dietary_rest", DB.String(50))
+    optional_info["num_hacks"] = DB.Column("num_hacks", DB.Integer)
+    optional_info["linkedin"] = DB.Column("linkedin", DB.String(50))
+    optional_info["github"] = DB.Column("github", DB.String(50))
+
+
 
     # Fill in the other attributes from DBSchema.md
 
-    def __init__(self, email='', fn='', ln='', univ=''):
+    def __init__(self, email, firstName, lastName, university, birthday, size, short_answer, **kwargs):
         # Still need public ID and private ID, generate them from unique email
         guid = uuid4()
         self.private_id = str(guid)
@@ -40,9 +55,16 @@ class Attendee(DB.Model):
 
         # general info
         self.email = email
-        self.first_name = fn
-        self.last_name = ln
-        self.school = univ
+        self.first_name = firstName
+        self.last_name = lastName
+        self.university = university
+        self.birthday = birthday
+        self.size = size
+        self. short_answer = short_answer
+
+        for key, value in kwargs.items():
+            self.optional_info[str(key)] = str(value)
+
 
     def __repr__(self):
         return "{ Attendee: email=%s, name=%s %s, school=%s }" % (self.email, self.first_name, self.last_name, self.school)

f0413ccc53f341b6964202f5d8b065932c5f42fd
Alex Lue <alue@ucsc.edu>
2018-10-13 18:35:57 -0700

    pylint for model/attendee.py

--- services/registration/models/attendee.py
+++ services/registration/models/attendee.py
@@ -5,14 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 DB = SQLAlchemy()
 
 class Attendee(DB.Model):
-    #pylint: disable=no-member,too-few-public-methods
+    # pylint: disable=no-member, too-few-public-methods, too-many-instance-attributes
     """Table of a people's submitted registration info."""
     __tablename__ = 'registrations'
 
     # required fields
-    private_id = DB.Column('private_id', DB.String(36), primary_key=True, nullable = False)
+    private_id = DB.Column('private_id', DB.String(36), primary_key=True, nullable=False)
     public_id = DB.Column('public_id', DB.Integer, unique=True, nullable=False)
-    email = DB.Column('email', DB.String(50), unique = True, nullable=False)
+    email = DB.Column('email', DB.String(50), unique=True, nullable=False)
     first_name = DB.Column('first_name', DB.String(20), nullable=False)
     last_name = DB.Column('last_name', DB.String(20), nullable=False)
     university = DB.Column('school', DB.String(50), nullable=False)
@@ -22,19 +22,19 @@ class Attendee(DB.Model):
 
     # optional fields
     optional_info = {}
-    optional_info["gender"] = DB.Column('gender', DB.String(1), nullable=True) #M or F or O (other or prefer not to say?)
-    optional_info["ethnicity"] = DB.Column('ethnicity',DB.String(20), nullable=True)
+
+    # M or F or O (other or prefer not to say?)
+    optional_info["gender"] = DB.Column('gender', DB.String(1), nullable=True)
+    optional_info["ethnicity"] = DB.Column('ethnicity', DB.String(20), nullable=True)
     optional_info["major"] = DB.Column('major', DB.String(20), nullable=True)
     optional_info["dietary_rest"] = DB.Column("dietary_rest", DB.String(50), nullable=True)
     optional_info["num_hacks"] = DB.Column("num_hacks", DB.Integer, nullable=True)
     optional_info["linkedin"] = DB.Column("linkedin", DB.String(50), nullable=True)
     optional_info["github"] = DB.Column("github", DB.String(50), nullable=True)
 
-
-
-    # Fill in the other attributes from DBSchema.md
-
+    # pylint: disable=line-too-long, too-many-arguments
     def __init__(self, email, firstName, lastName, university, birthday, size, short_answer, **kwargs):
+
         # Still need public ID and private ID, generate them from unique email
         guid = uuid4()
         self.private_id = str(guid)
@@ -56,4 +56,5 @@ class Attendee(DB.Model):
 
 
     def __repr__(self):
-        return "{ Attendee: email=%s, name=%s %s, school=%s }" % (self.email, self.first_name, self.last_name, self.school)
+        return "{ Attendee: email=%s, name=%s %s, school=%s }" % (
+            self.email, self.first_name, self.last_name, self.school)

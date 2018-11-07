bf7c5b5a0f7f08511c222aa43206c926cfc33e2b
Alex Lue <alue@ucsc.edu>
2018-11-07 10:52:40 -0800

    mergied mail_conf into mailing_list and renamed resource to MailingListConfirmation

--- services/registration/src/api/mailing_list.py
+++ services/registration/src/api/mailing_list.py
@@ -43,3 +43,31 @@ class MailingListSubmit(Resource):
         if not base.is_user_registered(MailingList, email):
             email = MailingList(email)
             base.commit_user(email)
+
+class MailingListConfirmation(Resource):
+    # pylint: disable=no-member, no-self-use
+    """Endpoint to add to email list and send email confirmation."""
+
+    @use_kwargs({
+        'email': fields.String(required=True)
+    })
+    def post(self, email):
+        """Sends confirmation in a post req
+        :param email: email to send to
+        :type  email: String
+
+        :param api_key: api_key to access mailchimp
+        :type  api_key: String
+
+        :returns: email success or error
+        : rtype : String
+        """
+
+        url = 'https://us17.api.mailchimp.com/3.0/lists/c566e13387/members'
+        request_made = requests.post(url, json={'email_address':email, 'status':'subscribed'},
+                                     auth=('user', os.environ['MAILCHIMP_APIK']))
+
+        if request_made.status_code == 404:
+            return "email error"
+
+        return "email success"

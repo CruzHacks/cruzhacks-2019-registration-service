4711f2e7ca28e5038c25a8f97f82a44e6ebc45e2
Alex Lue <alue@ucsc.edu>
2018-11-07 02:58:53 -0800

    pylint

--- services/registration/src/api/mail_conf.py
+++ services/registration/src/api/mail_conf.py
@@ -1,20 +1,19 @@
 import os
-import requests
 from flask import Flask, request, make_response, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route("/confirm", methods=["POST"])
 def sendconfirmation():
-   url = 'https://us17.api.mailchimp.com/3.0/lists/c566e13387/members'
-   email = request.form['email']
-   r = requests.post(url, json={'email_address':email, 'status':'subscribed'}, auth=('user', os.environ['MAIL_APIKEY']))
+    url = 'https://us17.api.mailchimp.com/3.0/lists/c566e13387/members'
+    email = request.form['email']
+    request_made = requests.post(url, json={'email_address':email, 'status':'subscribed'}, auth=('user', os.environ['MAIL_APIKEY']))
 
-   if r.status_code == 200:
-      return make_response(jsonify(msg="email sent!"), 200, {'Content-Type':'application/json'})
+    if request_made.status_code == 404:
+        return make_response(jsonify(msg="error"), 404, {'Content-Type':'application/json'})
+    
+    return make_response(jsonify(msg="email sent!"), 200, {'Content-Type':'application/json'})
 
-   else:
-      return make_response(jsonify(msg="error"), 404, {'Content-Type':'application/json'})
-
-if __name__== "__main__":
-   app.run(host=None, port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host=None, port=8080, debug=True)

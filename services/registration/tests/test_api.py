2c818cb9f2185ca2da49ef4c629157c56ad7b48d
Allston Mickey <allston.mickey@gmail.com>
2019-01-19 20:12:05 -0800

    Use side effects for test patches

--- services/registration/tests/test_api.py
+++ services/registration/tests/test_api.py
@@ -25,6 +25,7 @@ class TestWhitelist:
         FakeUser = namedtuple('FakeAccount', ['username', 'encrypted_password'])
         FakeRole = namedtuple('FakeRole', ['username'])
 
+        """
         # Implements the interface of a query from a table.
         class FakeUsernameQuery:
             def __init__(self, accounts):
@@ -62,6 +63,52 @@ class TestWhitelist:
         mocker.patch(
             'registration.src.api.utils.whitelist.Organizer'
         ).return_value = FakeAccount([])
+        """
+
+        def get_user(uid):
+            users = {
+                'amickey': FakeUser(
+                    username='amickey',
+                    encrypted_password=b'$2b$12$kVp2MoxWtYJhGWCT5fi4k.JH93YK6iHmaBo3fhfUB42eyrUJS.FNO' # p@ssw0rd
+                )
+                'yikes': FakeUser(
+                username='yikes',
+                encrypted_password=b'$2b$12$kVp2MoxWtYJhGWCT5fi4k.JH93YK6iHmaBo3fhfUB42eyrUJS.FNO' # p@ssw0rd
+                )
+            }
+            return users.get(uid)
+
+        def get_devs(uid):
+            roles = {
+                'amickey': FakeRole(username='amickey')
+            }
+            return roles.get(uid)
+      
+        def get_directors(uid):
+            roles = {
+                'yikes': FakeRole(username='yikes')
+            }
+            return roles.get(uid)
+
+        def get_organizers(uid):
+            roles = {}
+            return roles.get(uid)
+
+        mocker.patch(
+            'registration.src.api.utils.whitelist.User.query.get'
+        ).side_effect = get_user
+
+        mocker.patch(
+            'registration.src.api.utils.whitelist.Dev.query.get'
+        ).side_effect = get_devs
+
+        mocker.patch(
+            'registration.src.api.utils.whitelist.Director.query.get'
+        ).side_effect = get_directors
+
+        mocker.patch(
+            'registration.src.api.utils.whitelist.Organizers.query.get'
+        ).side_effect = get_organizers
 
     @staticmethod
     @verify({Dev})
